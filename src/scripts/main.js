b4f5fd7f58db53e1f5f900f4fb2fc6cf31385e92
Adam Byrd <ajbyrd0723@gmail.com>
2019-10-14 13:39:26 -0500

    file initialization

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+console.log("You will understand this.")
+
+adamPet = {
+    name: "Hank",
+    species: "Dog",
+    nicknames: ["Honk", "Henry", "LBD"],
+    age: 2,
+    bark: function () {
+        window.alert("WOOF")
+    },
+    hunt: function () {
+        window.alert("Go get em")
+    },
+    down: function () {
+        window.alert("lay down")
+    }
+}
+
+adamPet.hunt();
